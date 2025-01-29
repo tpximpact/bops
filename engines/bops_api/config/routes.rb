ec252e2c1e534d9e7de2e9a85501276f4d869307
gabimartini <gabimartinii@hotmail.com>
2025-01-29 16:18:12 +0000

    specialist comment

--- engines/bops_api/config/routes.rb
+++ engines/bops_api/config/routes.rb
@@ -34,6 +34,7 @@ BopsApi::Engine.routes.draw do
           get :search, on: :collection
           resource :documents, only: [:show]
           resource :comments, only: [:show]
+          resource :specialist, only: [:show]
         end
       end
     end
