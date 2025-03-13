4bb14565ebb1e4e82edc8984a53ad4d40fe827ee
gabimartini <gabimartinii@hotmail.com>
2025-03-13 08:46:23 +0000

    add suggestions

--- engines/bops_api/spec/requests/v2/public/comments_public_spec.rb
+++ engines/bops_api/spec/requests/v2/public/comments_public_spec.rb
@@ -17,19 +17,28 @@ RSpec.describe "BOPS public API" do
         description: "The planning application reference"
       }
 
-      parameter name: :sort_by, in: :path, schema: {
+      parameter name: :sortBy, in: :path, schema: {
         type: :string,
+        default: "received_at",
         description: "The sort type for the comments"
-      }
+      }, required: false
 
       parameter name: :order, in: :path, schema: {
         type: :string,
+        default: "asc",
         description: "The order for the comments"
-      }
-     parameter name: :maxResult, in: :path, schema: {
-        type: :string,
+      }, required: false
+
+     parameter name: :maxresults, in: :query, schema: {
+        type: :integer,
+        default: 10,
         description: "Max result for page"
-      }
+      }, required: false
+
+     parameter name: :page, in: :query, schema: {
+        type: :integer,
+        default: 1
+      }, required: false
 
       response "200", "returns a planning application's specialist comments given a reference" do
         example "application/json", :default, example_fixture("comments.json")
@@ -40,13 +49,15 @@ RSpec.describe "BOPS public API" do
           create(:neighbour_response, neighbour: create(:neighbour, consultation: planning_application.consultation))
         end
 
-        let(:sort_by) { 'received_at' }
+        let(:page) { 1 }
+        let(:sortBy) { 'received_at' }
         let(:order) { 'desc' }
 
         run_test! do |response|
           data = JSON.parse(response.body)
           puts data.inspect
           expect(data["metadata"]["totalResults"]).to eq(1)
+          expect(data["metadata"]["page"]).to eq(1)
           expect(data["data"].count).to eq(1)
         end
 
