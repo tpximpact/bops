0f220755308b39a8e24a2de4d6c8cfc1e7f85b6a
gabimartini <gabimartinii@hotmail.com>
2025-07-01 15:19:07 +0100

    fix lint

--- engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
+++ engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
@@ -32,36 +32,36 @@ module BopsApi
 
         def required_api_key_scope = "comment"
 
-          private
+        private
 
-          def build_neighbour_response
-            @consultation.neighbour_responses.build(response_attributes).tap do |response|
-              response.neighbour = find_or_create_neighbour
-            end
+        def build_neighbour_response
+          @consultation.neighbour_responses.build(response_attributes).tap do |response|
+            response.neighbour = find_or_create_neighbour
           end
+        end
 
-          def find_or_create_neighbour
-            @consultation.neighbours.find_by(address: params[:address]) ||
-              @consultation.neighbours.build(
-                address: params[:address],
-                selected: false,
-                source: "sent_comment"
-              )
-          end
-
-          def response_attributes
-            response_params.except(:address, :planning_application_id).merge!(
-              received_at: Time.zone.now,
-              consultation_id: @consultation.id
+        def find_or_create_neighbour
+          @consultation.neighbours.find_by(address: params[:address]) ||
+            @consultation.neighbours.build(
+              address: params[:address],
+              selected: false,
+              source: "sent_comment"
             )
-          end
+        end
 
-          def response_params
-            params.permit(
-              :address, :name, :email, :response, :summary_tag,
-              :redacted_response, tags: []
-            )
-          end
+        def response_attributes
+          response_params.except(:address, :planning_application_id).merge!(
+            received_at: Time.zone.now,
+            consultation_id: @consultation.id
+          )
+        end
+
+        def response_params
+          params.permit(
+            :address, :name, :email, :response, :summary_tag,
+            :redacted_response, tags: []
+          )
+        end
       end
     end
   end
